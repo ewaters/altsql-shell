b2264008f58092d7dffd02dae61db20e5421d122
Eric Waters <ewaters@shutterstock.com>
2012-04-10 10:18:52 -0400

    Add lots of nifty features

--- lib/MySQL/ANSIClient.pm
+++ lib/MySQL/ANSIClient.pm
@@ -13,7 +13,15 @@ use Time::HiRes qw(gettimeofday tv_interval);
 ## Configure
 
 sub help_text {
+	return <<EOF;
 
+  -h --host HOSTNAME
+  -u --user USERNAME
+  -p --pass PASSWORD
+  -P --port PORT
+  -d --database DATABASE
+  -? --help
+EOF
 }
 
 sub capture_command_line_args {
@@ -26,20 +34,14 @@ sub capture_command_line_args {
 		'database|d=s' => \$args{database},
 		'port|P=s' => \$args{port},
 		'help|?'   => \$args{help},
+		'history=s' => \$args{_term_history_fn},
 	);
 	if (@ARGV && int @ARGV == 1) {
 		$args{database} = $ARGV[0];
 	}
+	$args{_term_history_fn} ||= $ENV{HOME} . '/.mysqlc_history.js';
 	if ($args{help}) {
-		print <<EOF;
-
-  -h --host HOSTNAME
-  -u --user USERNAME
-  -p --pass PASSWORD
-  -P --port PORT
-  -d --database DATABASE
-  -? --help
-EOF
+		print $class->help_text();
 		exit;
 	}
 	return %args;
@@ -47,14 +49,42 @@ EOF
 
 sub new {
 	my $class = shift;
-	my %args = validate(@_, {
-		term_class => { default => 'MySQL::ANSIClient::Term' },
+	my %self = validate(@_, {
+		term_class  => { default => 'MySQL::ANSIClient::Term' },
 		table_class => { default => 'MySQL::ANSIClient::Table' },
 		args => 1,
 	});
-	my $self = bless \%args, $class;
+	my $self = bless \%self, $class;
 
 	$self->db_connect();
+
+	## Create a term
+
+	# Extract out term args from args
+	$self->{term_args} = {
+		map { my $key = $_; /^_term_(.+)/; +($1 => delete $self->{args}{$key}) }
+		grep { /^_term_/ }
+		keys %{ $self->{args} }
+	};
+
+	my $term_class = $self->{term_class};
+	eval "require $term_class";
+	die $@ if $@;
+	$self->{term} = $term_class->new(
+		app => $self,
+		%{ $self->{term_args} },
+	);
+
+	## Create a table render
+
+	my $table_class = $self->{table_class};
+	eval "require $table_class";
+	die $@ if $@;
+	$self->{table} = $table_class->new($self);
+
+	## Create other reusable objects
+
+	$self->{sql_parser} = DBIx::MyParsePP->new();
 
 	return $self;
 }
@@ -62,13 +92,29 @@ sub new {
 sub db_connect {
 	my $self = shift;
 	my $dsn = 'DBI:mysql:' . join (';',
-		map { "$_;$self->{args}{$_}" }
+		map { "$_=$self->{args}{$_}" }
 		grep { defined $self->{args}{$_} }
 		qw(database host port)
 	);
 	$self->{dbh} = DBI->connect($dsn, $self->{args}{user}, $self->{args}{password}, {
 		PrintError => 0,
-	}) or die $DBI::errstr . "\n";
+	}) or die $DBI::errstr . "\nDSN used: '$dsn'\n";
+
+	my %types;
+	my $type_info_all = $self->{dbh}->type_info_all();
+	my %key_map = %{ shift @$type_info_all };
+
+	$types{unknown} = { map { $_ => 'unknown' } keys %key_map };
+
+	foreach my $i (0..$#{ $type_info_all }) {
+		my %type;
+		while (my ($key, $index) = each %key_map) {
+			$type{$key} = $type_info_all->[$i][$index];
+		}
+		$types{$i} = \%type;
+	}
+
+	$self->{db_types} = \%types;
 }
 
 sub new_from_cli {
@@ -90,26 +136,15 @@ sub run {
 
 	$self->log_info("Starting ".__PACKAGE__);
 
-	## Create a terminal and begin to read from it
-
-	my $term_class = $self->{term_class};
-	eval "require $term_class";
-	die $@ if $@;
-	my $term = $term_class->new($self);
-
-	my $table_class = $self->{table_class};
-	eval "require $table_class";
-	die $@ if $@;
-	my $table = $table_class->new($self);
-
-	my $parser = DBIx::MyParsePP->new();
-
 	my $input;
-	while (defined ($input = $term->readline())) {
+	while (defined ($input = $self->{term}->readline())) {
 		# Next if Ctrl-C or if user typed nothing
 		if (! length $input) {
 			next;
 		}
+
+		$input =~ s/\s*$//; # no trailing spaces
+		$input =~ s/;*$//;  # no trailing semicolon
 
 		# Extract out \G
 		my %render_opts;
@@ -117,53 +152,107 @@ sub run {
 			$render_opts{one_row_per_table} = 1;
 		}
 
-		# Attempt to parse the input with a SQL parser
-		my $parsed = $parser->parse($input);
-		if (! defined $parsed->root) {
-			$self->log_error(sprintf "Error at pos %d, line %s", $parsed->pos, $parsed->line);
+		# Allow the user to pass non-SQL control verbs
+		if ($input =~ m/^\s*(quit|exit)\s*$/) {
+			$self->shutdown();
+		}
+
+		# Allow the user to execute perl code via '% print Dumper(...);'
+		if (my ($perl_code) = $input =~ m/^% (.+)$/) {
+			eval $perl_code;
+			if ($@) {
+				$self->log_error($@);
+			}
 			next;
 		}
 
-		# Based on the context of what type of action is being performed, print a table or print just a success/failure
-		my $statement = $parsed->root->extract('statement');
-		if (! $statement) {
-			$self->log_error("Not sure what to do with this; no 'statement' in the parse tree");
-			next;
-		}
+		$self->handle_sql_input($input, \%render_opts);
+	}
+}
 
-		my $sql = $parsed->toString;
-		given (my $verb = $statement->children->[0]) {
-			when [qw( select describe explain show )] {
-				my %data = (
-					timing => { start => scalar gettimeofday },
-				);
+sub shutdown {
+	my $self = shift;
 
-				my $sth = $self->{dbh}->prepare($sql);
-				$sth->execute();
+	$self->{term}->write_history();
 
-				foreach my $i (0..$sth->{NUM_OF_FIELDS} - 1) {
-					push @{ $data{columns} }, {
-						name      => $sth->{NAME}[$i],
-						type      => $sth->{TYPE}[$i],
-						precision => $sth->{PRECISION}[$i],
-						scale     => $sth->{SCALE}[$i],
-						nullable  => $sth->{NULLABLE}[$i] || undef,
-					};
-				}
-				
-				$data{rows} = $sth->fetchall_arrayref;
-				$data{timing}{stop} = scalar gettimeofday;
+	exit;
+}
 
-				$table->render(\%data);
-			}
-			when [qw( use create )] {
-				$self->{dbh}->do($sql);
-			}
-			default {
-				$self->log_info("I don't know how to handle '$verb'");
-			}
+sub handle_sql_input {
+	my ($self, $input, $render_opts) = @_;
+
+	# Attempt to parse the input with a SQL parser
+	my $parsed = $self->{sql_parser}->parse($input);
+	if (! defined $parsed->root) {
+		$self->log_error(sprintf "Error at pos %d, line %s", $parsed->pos, $parsed->line);
+		return;
+	}
+
+	# Based on the context of what type of action is being performed, print a table or print just a success/failure
+	my $statement = $parsed->root->extract('statement');
+	if (! $statement) {
+		$self->log_error("Not sure what to do with this; no 'statement' in the parse tree");
+		return;
+	}
+
+	my $sql = $parsed->toString;
+
+	my $output_type = 'query';
+	my $verb = $statement->children->[0];
+	given ($verb) {
+		when [qw( select describe explain show )] { $output_type = 'table' }
+		when [qw( use create alter update insert delete )]      { $output_type = 'query' }
+		default {
+			$self->log_info("I don't know how to handle '$verb'; assume non-table output");
 		}
 	}
+	
+	my %data;
+	$data{timing}{start} = gettimeofday;
+
+	my $sth = $self->{dbh}->prepare($sql);
+	$sth->execute();
+	if (my $error = $self->{dbh}->errstr) {
+		$self->log_error($error);
+		return;
+	}
+
+	$data{timing}{stop} = gettimeofday;
+
+	if ($output_type eq 'table') {
+		foreach my $i (0..$sth->{NUM_OF_FIELDS} - 1) {
+			push @{ $data{columns} }, {
+				name      => $sth->{NAME}[$i],
+				type      => $sth->{TYPE}[$i],
+				precision => $sth->{PRECISION}[$i],
+				scale     => $sth->{SCALE}[$i],
+				nullable  => $sth->{NULLABLE}[$i] || undef,
+			};
+		}
+		$data{rows} = $sth->fetchall_arrayref;
+		$self->{table}->render(\%data, $render_opts);
+	}
+	elsif ($verb eq 'use') {
+		$self->log_info('Database changed');
+	}
+	else {
+		$self->log_info(sprintf 'Query OK, %d row%s affected (%.2f sec)', $sth->rows, ($sth->rows > 1 ? 's' : ''), $data{timing}{stop} - $data{timing}{start});
+		if ($verb ne 'insert') {
+			$self->log_info(sprintf 'Records: %d  Warnings: %d', $sth->rows, $sth->{mysql_warning_count});
+		}
+		$self->log_info(''); # empty line
+	}
+}
+
+sub db_type_info {
+	my ($self, $type) = @_;
+
+	my $info = $self->{db_types}{$type};
+	if (! $info) {
+		#$self->log_error("No such type info for $type");
+		return $self->{db_types}{unknown};
+	}
+	return $info;
 }
 
 ## Output display
--- lib/MySQL/ANSIClient/Term.pm
+++ lib/MySQL/ANSIClient/Term.pm
@@ -4,24 +4,25 @@ use strict;
 use warnings;
 use Term::ReadLine::Zoid;
 use Data::Dumper;
+use JSON::XS;
 
 sub new {
-	my ($class, $app) = @_;
+	my ($class, %self) = @_;
 
 	my $term = Term::ReadLine::Zoid->new("mysql-color");
-	my %self = (
-		term => $term,
-		app  => $app,
-	);
+	$self{term} = $term;
 	my $self = bless \%self, $class;
 
-	$self->{app}->log_info("Ctrl-C to reset the line; Ctrl-D on an empty line to exit");
+	$self->{app}->log_info("Ctrl-C to reset the line; Ctrl-D to exit");
 
 	$term->Attribs->{completion_function} = sub {
 		$self->completion_function(@_);
 	};
 
-	$term->Attribs->{default_mode} = 'multiline';
+	$term->bindkey('^D', sub {
+		print "\n";
+		$self->{app}->shutdown();
+	});
 
 	## The user has pressed the 'enter' key.  If the buffer ends in ';' or '\G', accept the buffer
 	$term->bindkey('return', sub {
@@ -33,6 +34,8 @@ sub new {
 			$term->insert_line();
 		}
 	});
+
+	$self->read_history();
 
 	return $self;
 }
@@ -49,6 +52,41 @@ sub completion_function {
 	$self->{app}->log_debug("\ncompletion_function: '$word', '$buffer', '$start'");
 
 	return ();
+}
+
+sub write_history {
+	my ($self, $fn) = @_;
+
+	$fn ||= $self->{history_fn};
+	if (! $fn) {
+		return;
+	}
+
+	open my $out, '>', $fn or die "Can't open $fn for writing: $!";
+	print $out encode_json({ history => [ $self->{term}->GetHistory ] });
+	close $out;
+}
+
+sub read_history {
+	my ($self, $fn) = @_;
+
+	# Seed the history from a file if present
+	$fn ||= $self->{history_fn};
+	if (! $fn || ! -f $fn) {
+		return;
+	}
+
+	open my $in, '<', $fn or die "Can't open $fn for reading: $!";
+	local $\ = undef;
+	my $data = <$in>;
+	close $in;
+	eval {
+		my $parsed = decode_json($data);
+		$self->{term}->SetHistory(@{ $parsed->{history} });
+	};
+	if (my $exception = $@) {
+		$self->{app}->log_error("An error occurred when decoding $fn: $exception");
+	}
 }
 
 1;
