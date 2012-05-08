bd2aa5549a3cc78219d79b1e470e349daac18fc3
Eric Waters <ewaters@shutterstock.com>
2012-05-08 17:32:22 -0400

    Split lines in the table output

--- lib/App/AltSQL/View/Plugin/UnicodeBox.pm
+++ lib/App/AltSQL/View/Plugin/UnicodeBox.pm
@@ -6,7 +6,9 @@ use Text::UnicodeBox::Table;
 sub _render_table_data {
 	my ($self, $data) = @_;
 
-	my $table = Text::UnicodeBox::Table->new();
+	my $table = Text::UnicodeBox::Table->new(
+		split_lines => 1,
+	);
 
 	$table->add_header({ style => 'heavy' }, @{ $data->{columns} });
 	$table->add_row(@$_) foreach @{ $data->{rows} };
